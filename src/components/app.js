a21924e8727f3413a4cbe58fc9341081e4fbb2a0
fitrinanur <missfitrina@gmail.com>
2017-10-04 11:53:16 +0700

    Setting Config

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import SearchBar from './search_bar';
+import VideoList from './video_list';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>React simple starter
+        <SearchBar/>
+        <VideoList />
+      </div>
     );
   }
 }
